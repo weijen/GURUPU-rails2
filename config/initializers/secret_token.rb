1fa6d0321ca9d37b4abaef8605ddb237666eda1b
Wei Jen Lu <weijenlu@gmail.com>
2012-05-08 11:52:05 +0800

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gurupu::Application.config.secret_token = 'a53257aee77b54602874911d1d6ff07d6e1ac55d33e8a3d83bb3d37ffffc40a89e30fadbd2657c8f4152d1878ed91e80e9ccc3d22f1796a743219bf1939b3d5a'
